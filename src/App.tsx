4091dbf39033ccdd9d4ec2aa853a8dce27aa886d
schokett <skrollanelenazimmermann@gmail.com>
2026-07-02 01:25:23 +0200

    styling design

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="fixed inset-0 -z-10 pointer-events-none">
         <div className="bg-orange-300 rounded-full mx-auto w-25 h-25 -mt-7 -top-2 -right-5 lg:w-40 lg:h-40 lg:-mt-7 lg:-top-10 lg:-right-10 absolute "></div>
         <div className="bg-pink-300  rounded-3xl mx-auto rotate-15 w-22 h-22 mt-10 -left-8 lg:w-32 lg:h-32  lg:mt-10 lg:-left-10 absolute"></div>
-        <div className="bg-green-300 mx-auto -rotate-15 [clip-path:polygon(50%_30%,0%_100%,100%_100%)] w-5 h-5  left-22 lg:w-8 lg:h-8 mt-15 lg:left-40 absolute"></div>
+        <div className="bg-emerald-400 mx-auto -rotate-15 [clip-path:polygon(50%_30%,0%_100%,100%_100%)] w-5 h-5  left-22 lg:w-8 lg:h-8 mt-15 lg:left-40 absolute"></div>
         <div className="border-pink-400 rounded-full border-4 bg-transparent w-5 h-5 right-18 mt-25 lg:w-6 lg:h-6 lg:right-22 lg:mt-35 absolute"></div>
         <div className="bg-blue-400 mx-auto -rotate-45 w-4 h-4 mt-90 left-20 lg:w-8 lg:h-8 lg:mt-130 lg:left-30 absolute"></div>
         <div className="bg-orange-300 rounded-full -rotate-20 mx-auto w-6 h-2 mt-89 right-20 lg:w-10 lg:h-3 lg:mt-125 lg:right-60  absolute"></div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,37 @@
 function Home() {
-  return <div className="text-3xl font-bold text-center">Home</div>;
+  return (
+    <div className=" text-center ">
+      <div className="flex justify-between items-center p-4">
+        <div className="flex items-center gap-2">
+          <div className="bg-emerald-600 rounded-xl p-1 -rotate-10">
+            <img
+              src="/logo.svg"
+              alt="Logo"
+              className="w-10 h-auto drop-shadow-md drop-shadow-slate-500/50"
+            />
+          </div>
+          <p className="text-md font-medium text-neutral-700">hyodo.dev</p>
+        </div>
+
+        <nav className="gap-2 flex font-medium text-xs">
+          <div className="bg-green-200 text-green-950 rounded-full p-1 px-2">
+            <a href="" className="">
+              projekte
+            </a>
+          </div>
+          <div className="bg-red-200 text-red-950 rounded-full p-1 px-2">
+            <a href="" className="über mich">
+              über mich
+            </a>
+          </div>
+          <div className="bg-indigo-200 text-indigo-950 rounded-full p-1 px-2">
+            <a href="" className="kontakt">
+              kontakt
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 export default Home;

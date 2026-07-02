de7105a79afebb2519c678e5624b51d0a366e97a
schokett <skrollanelenazimmermann@gmail.com>
2026-07-02 02:23:11 +0200

    deploy

--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,18 @@ import App from "./App.tsx";
 import Home from "./pages/Home.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "*", element: <NotFound /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "*", element: <NotFound /> },
-    ],
+    basename: "/hyodoWebsite/",
   },
-]);
+);
